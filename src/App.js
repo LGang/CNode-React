3f2623944111468a2a50bd75948e01d2111e5e85
Leo <gang.li@huijie-inc.com>
2018-11-12 18:45:44 +0800

    stage

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import './App.css'
 
 import React, {Component} from 'react'
+import {HashRouter as Router, Route, Switch} from 'react-router-dom'
 
-class App extends Component {
+import Index from './pages/Index'
+
+export default class App extends Component {
   render() {
-    return <div className="App">
-      <div className="App-heading App-flex">
-        <h2>Welcome to <span className="App-react">React</span></h2>
-      </div>
-      <div className="App-instructions App-flex">
-        <img className="App-logo" src={require('./react.svg')}/>
-        <p>Edit <code>src/App.js</code> and save to hot reload your changes.</p>
-      </div>
-    </div>
+    return (
+      <Router>
+        <Switch>
+          {/* 路由配置 */}
+          <Route exact path="/" component={Index} />
+        </Switch>
+      </Router>
+    )
   }
 }
-
-export default App
--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import './index.css'
 import React from 'react'
 import {render} from 'react-dom'
 
+import './style/Index.less'
+
 import App from './App'
 
 render(<App/>, document.querySelector('#app'))
