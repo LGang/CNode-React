3b8dc5e759c0b6e139ff3d1c279e3e141078935d
Leo <gang.li@huijie-inc.com>
2018-11-13 18:42:05 +0800

    style lasted editor avatar & lasted edit time

--- src/pages/Cell.js
+++ src/pages/Cell.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
 import '../style/Cell.less'
+import {timeAgoSinceNow} from "../util/util"
 
 export class Cell extends React.Component {
 
@@ -37,7 +38,7 @@ export class Cell extends React.Component {
                 <div className="right-party">
                     <Link to="profile">
                         <img src={this.props.author.avatar_url} />
-                        <span className="last-reply-time">{this.props.last_reply_at}</span>
+                        <span className="last-reply-time">{timeAgoSinceNow(this.props.last_reply_at)}</span>
                     </Link>
                 </div>
             </div>
---
+++ src/util/util.js
@@ -0,0 +1,22 @@
+
+function timeAgoSinceNow(date) {
+    let createDate = new Date(date)
+    let currentDate = new Date()
+    let createDateTimeStamp = createDate.getTime()
+    let currentDateTimeStamp = currentDate.getTime()
+    let timeSinceNow = Math.floor((currentDateTimeStamp - createDateTimeStamp) / 1000)
+
+    let years = Math.floor(timeSinceNow / (60*60*24*365))
+    if (years) { return years + "年前"} 
+    let months = Math.floor(timeSinceNow / (60*60*24*30))
+    if (months) { return months + "月前"} 
+    let days = Math.floor(timeSinceNow / (60*60*24))
+    if (days) { return days + "天前"} 
+    let hours = Math.floor(timeSinceNow / (60*60))
+    if (hours) { return hours + "小时前"} 
+    let minutes = Math.floor(timeSinceNow / 60)
+    if (minutes) { return minutes + "分钟前"} 
+    return timeSinceNow + "秒前"
+}
+
+export {timeAgoSinceNow}
